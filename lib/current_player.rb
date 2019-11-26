c77718095f861d1dff7b6f4fc2aa130454d1e8b3
Brandon Nightingale <brandon@jbnight.com>
2019-11-26 05:44:37 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,10 @@
 
 def turn_count(board)
   counter = 0
-  if (board[index]) == "X" || (board[index]) == "O"
   board.each do |turn|
+    if turn == "X" || turn == "O"
       counter += 1
       return "#{counter}"
+    end
   end
 end
