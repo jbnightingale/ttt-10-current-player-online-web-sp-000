f145ae35167617e145c5a63a32b4ce59d5d46c52
Brandon Nightingale <brandon@jbnight.com>
2019-11-26 06:02:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,4 @@
 
-
 def turn_count(board)
   counter = 0
   board.each do |turn|
@@ -8,4 +7,11 @@ def turn_count(board)
     end
   end
   return counter
+end
+
+def current_player(board)
+  if current_player(board).even? == true
+    return "X"
+  elsif current_player(board).odd? == true
+    return "O"
 end
